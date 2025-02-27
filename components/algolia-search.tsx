27070a3ec0767ca6e4e87f98b0aeca42d53a00d0
ashkansamadiyan <ashkan.samadiyan@gmail.com>
2025-02-27 16:27:29 +0330

    blog posts

--- components/algolia-search.tsx
+++ components/algolia-search.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { DocSearch } from "@docsearch/react";
-import "@docsearch/css";
 
-type AlgoliaProps = {
-  appId: string;
-  indexName: string;
-  apiKey: string;
-};
-
-export default function AlgoliaSearch(props: AlgoliaProps) {
+export default function AlgoliaSearch() {
   return (
     <div className="relative border rounded-lg sm:w-fit w-[68%]">
       <div className="absolute right-2 top-[0.4rem] hidden items-center gap-0.5 text-xs font-code sm:flex pointer-events-none">
@@ -20,7 +12,6 @@ export default function AlgoliaSearch(props: AlgoliaProps) {
           K
         </div>
       </div>
-      <DocSearch {...props} maxResultsPerGroup={5} />
       <div className="absolute top-[0.42rem] left-24 text-muted-foreground font-code -tracking-[0.1rem] text-sm pl-[0.081rem] pointer-events-none">
         ...
       </div>
